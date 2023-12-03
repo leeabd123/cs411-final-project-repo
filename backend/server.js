9c5f71ef6501c240ed7c6aa2cc8eece428ecf53f
leeabd123 <labde2@illinois.edu>
2023-12-02 20:29:56 -0600

    files

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,46 @@
 const express = require('express');
+const mysql = require('mysql2');
+
+// Initialize express app
 const app = express();
 
 // Use environment variable for port or default to 3000
 const port = process.env.PORT || 3000;
 
+// Create a MySQL pool connection
+const pool = mysql.createPool({
+  host: '34.41.207.129',
+  user: 'root',
+  password: '', // Use environment variable for the password
+  database: 'cs411',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+// Function to check database connection
+function checkDatabaseConnection(req, res, next) {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      return res.status(500).send('Error connecting to the database');
+    }
+    
+    console.log('Successfully connected to the database');
+    connection.release();
+    next();
+  });
+}
+
+// Use the database connection check middleware
+app.use(checkDatabaseConnection);
+
+// Simple route for testing
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// Start the Express server
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,32 +2,6 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const express = require('express');
-const mysql = require('mysql2');
-
-const pool = mysql.createPool({
-  host: '34.41.207.129',
-  user: 'root',
-  password: '',
-  database: 'CS411-Project',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
-
-pool.getConnection((err, connection) => {
-  if (err) {
-    if (err.code === 'PROTOCOL_CONECTION_LOST') {
-      console.error('Database connection was closed.');
-    }
-    if (err.code === 'ER_CON_COUNT_ERROR') {
-      console.error('Database has too many connections.');
-    }
-    if (err.code === 'ECONNREFUSED') {
-      console.error('Database connection was refused.');
-    }
-  }
-});
 
 const App = () => {
   const [state, setState] = useState('');
@@ -48,7 +22,7 @@ const App = () => {
     try {
       // Replace this URL with the endpoint of backend server
       //const response = await axios.get('url`);
-      setEvents(response.data);
+      // setEvents(response.data);
     } catch (error) {
       console.error('Error fetching weather events:', error);
     }
