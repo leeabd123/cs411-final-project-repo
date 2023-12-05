a432ce0c1f2e25ebc4986e1b56e300dcd0b5a6d5
leeabd123 <labde2@illinois.edu>
2023-12-04 22:32:22 -0600

    so far everything except additonal categroy endpoints work so far

--- backend/routes/statistics.js
+++ backend/routes/statistics.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const statisticsController = require('../models/statistics');
 
+//this works 
+
 // Total number of deaths for each weather event category
 router.get('/total-deaths-by-category', statisticsController.totalDeathsByCategory);
 
